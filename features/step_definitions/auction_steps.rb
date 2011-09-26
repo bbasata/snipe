f093ec1a3329359063b89d1e79f0c142f5b8a759
Baraa Basata <bbasata@gmail.com>
2011-09-25 23:14:54 -0400

    Time for 'Some Initial Choices' [page 86]

---
+++ features/step_definitions/auction_steps.rb
@@ -0,0 +1,16 @@
+Given /^the auction has started selling an item$/ do
+    pending 
+end
+
+Given /^I start bidding in the auction$/ do
+    pending 
+end
+
+When /^the auction closes$/ do
+    pending 
+end
+
+Then /^I see that I have lost the auction$/ do
+    pending 
+end
+
