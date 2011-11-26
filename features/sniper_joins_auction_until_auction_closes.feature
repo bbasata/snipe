c832867ebd8476bb9dfb3a2de9de2905eb01fb1a
Baraa Basata <bbasata@gmail.com>
2011-11-26 14:59:22 -0500

    First feature is green, and the auction & sniper are on speaking terms

--- features/sniper_joins_auction_until_auction_closes.feature
+++ features/sniper_joins_auction_until_auction_closes.feature
@@ -4,5 +4,6 @@ Feature: Sniper joins auction until auction closes
   Scenario:
     Given the auction has started selling an item
     And I start bidding in the auction
+    Then the auction receives a join request from me
     When the auction closes
     Then I see that I have lost the auction
--- features/step_definitions/auction_steps.rb
+++ features/step_definitions/auction_steps.rb
@@ -1,4 +1,4 @@
-class FakeAuctionServer
+class Auction
   def initialize(item_id)
   end
 
@@ -8,33 +8,64 @@ class FakeAuctionServer
 
   def stop_selling_item
     @selling = false
+    @announcer.call
   end
 
   def selling?
     @selling
   end
+
+  def join(&announcer)
+    @joined = true
+    @announcer = announcer
+  end
+
+  def has_received_join_request?
+    @joined ||= false
+  end
 end
 
+class Sniper
+  def start_bidding_in(auction)
+    auction.join do
+      receive_message
+    end
+  end
+
+  def receive_message
+    @status ||= "Lost"
+  end
+
+  def status
+    "Won"
+  end
+end
+
+module KnowsTheDomain
+  def sniper; @sniper ||= Sniper.new; end
+  def auction; @auction ||= Auction.new("item-54321"); end
+end
+
+World(KnowsTheDomain)
+
 Given /^the auction has started selling an item$/ do
-  @auction_server = FakeAuctionServer.new("item-54321")
-  @auction_server.start_selling_item
-  @auction_server.should be_selling
+  auction.start_selling_item
+  auction.should be_selling
 end
 
 Given /^I start bidding in the auction$/ do
-  visit '/'
-  click_button 'Start Bidding'
+  sniper.start_bidding_in(auction)
 end
 
 When /^the auction closes$/ do
-  @auction_server.stop_selling_item
-  @auction_server.should_not be_selling
+  auction.stop_selling_item
+  auction.should_not be_selling
+end
+
+Then /^the auction receives a join request from me$/ do
+  auction.should have_received_join_request
 end
 
 Then /^I see that I have lost the auction$/ do
-  page.should have_content('Lost')
-end
-
-After do |scenario|
-  save_and_open_page if scenario.failed?
+  sniper.status.should == 'Lost'
 end
